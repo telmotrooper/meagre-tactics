fe9167a5b503e09464428c82298a6db4ea97c6c3
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-10 21:11:58 -0300

    only highlight tile if a piece is there

--- tile.gd
+++ tile.gd
@@ -7,7 +7,8 @@ extends CSGBox3D
 @export var walk_tile_material: Material
 
 func _on_area_3d_mouse_entered() -> void:
-	material = hover_tile_material
+	if len($Area3D2.get_overlapping_bodies()) > 0:
+		material = hover_tile_material
 
 func _on_area_3d_mouse_exited() -> void:
 	material = tile_material
