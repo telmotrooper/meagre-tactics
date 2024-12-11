43d0f87428c9603e015836afd35f23530b1b99f1
Telmo "Trooper <telmo.valverde@tarmac.io>
2024-12-10 22:27:13 -0300

    pieces can have any of two colors (#3)
    * set a black background
    
    * a few camera adjustments
    
    * added blue material
    
    * added red material
    
    * pieces can have any of two colors
    
    * added global state
    
    * reorganizing piece scene
    
    * adjusting 'eyes' in piece
    
    * added some ground work to mark tiles as walkable
    
    * added 'build and deploy' script

---
+++ game_state.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var selected_piece: Piece
--- piece/piece.gd
+++ piece/piece.gd
@@ -1,4 +1,14 @@
+class_name Piece
 extends CharacterBody3D
 
+@export var color := "blue"
+
+@export_group("Materials")
+@export var blue_material: Material
+@export var red_material: Material
+
 func _ready() -> void:
-	pass
+	if color == "blue":
+		$Headband.set_surface_override_material(0, blue_material)
+	elif color == "red":
+		$Headband.set_surface_override_material(0, red_material)
