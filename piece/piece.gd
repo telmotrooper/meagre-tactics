c909134d1f070f693a2c62d8fa3214b50ad6e1d3
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-10 21:55:22 -0300

    pieces can have any of two colors

--- piece/piece.gd
+++ piece/piece.gd
@@ -1,4 +1,13 @@
 extends CharacterBody3D
 
+@export var color := "blue"
+
+@export_group("Materials")
+@export var blue_material: Material
+@export var red_material: Material
+
 func _ready() -> void:
-	pass
+	if color == "blue":
+		$MeshInstance3D3.set_surface_override_material(0, blue_material)
+	elif color == "red":
+		$MeshInstance3D3.set_surface_override_material(0, red_material)
