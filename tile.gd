409d73d938050711969b114a37ab4b29c6084d40
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-12 22:18:48 -0300

    added enum to represent state in tiles

--- tile.gd
+++ tile.gd
@@ -6,7 +6,9 @@ extends CSGBox3D
 @export var hover_tile_material: Material
 @export var walk_tile_material: Material
 
-var walkable := false
+enum State { REGULAR, WALKABLE, HOVER }
+
+var state := State.REGULAR
 
 func has_piece() -> bool:
 	return %PieceDetector.get_collider() != null
@@ -18,11 +20,12 @@ func is_left_mouse_click(event: InputEvent) -> bool:
 	return event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed
 
 func _on_area_3d_mouse_entered() -> void:
-	if has_piece() and not walkable:
+	if has_piece() and state != State.WALKABLE:
+		state = State.HOVER
 		material = hover_tile_material
 
 func _on_area_3d_mouse_exited() -> void:
-	if not walkable:
+	if state != State.WALKABLE:
 		material = tile_material
 
 func _on_area_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
@@ -34,10 +37,11 @@ func _on_area_3d_input_event(_camera: Node, event: InputEvent, _event_position: 
 				ray_cast.get_collider().get_tile().set_as_walkable()
 
 func reset_mode() -> void:
-	walkable = false
-	material = tile_material
+	if state != State.HOVER:
+		state = State.REGULAR
+		material = tile_material
 
 func set_as_walkable() -> void:
 	if not has_piece():
-		walkable = true
+		state = State.WALKABLE
 		material = walk_tile_material
