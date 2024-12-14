69bccd9a5c2727561fcde209b3417952eed45e67
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-14 19:48:10 -0300

    initial implementation for 'unit setup'

---
+++ unit/unit_setup.gd
@@ -0,0 +1,8 @@
+class_name UnitSetup
+extends Resource
+
+enum MovementType { NEIGHBORING_TILES }
+
+@export var unit_name := "Unit Name"
+@export var movement_type := MovementType.NEIGHBORING_TILES
+@export var movement_range := 1
