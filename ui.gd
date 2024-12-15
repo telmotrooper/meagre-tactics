24c1ab11bb9a621db0413da0796de94704728f50
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-14 23:42:11 -0300

    display unit color in overview

--- ui.gd
+++ ui.gd
@@ -1,13 +1,13 @@
 extends Control
 
-const text = "%s\nHP: %d/%d"
+const text = "%s (%s)\nHP: %d/%d"
 
 func _ready() -> void:
 	GameState.unit_hovered.connect(update_unit_overview)
 	GameState.not_hovering_any_unit.connect(fallback_overview)
 
 func update_unit_overview(unit: Unit) -> void:
-	%SelectedUnitOverview.text = text % [unit.unit_type.unit_name, unit.current_hp, unit.unit_type.max_hp]
+	%SelectedUnitOverview.text = text % [unit.unit_type.unit_name, unit.color.capitalize(), unit.current_hp, unit.unit_type.max_hp]
 
 func fallback_overview() -> void:
 	if GameState.selected_unit:
