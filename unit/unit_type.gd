d345b1e18143f92ace0fe4cef331339a84446d91
Telmo "Trooper <telmo.valverde@tarmac.io>
2024-12-18 08:32:15 -0300

    added ui buttons (#15)
    * added attack-related fields to unit type
    
    * added attack-related states to tile
    
    * added default text to 'unit overview'
    
    * added ui icons
    
    * focus walk button
    
    * added button to end turn

--- unit/unit_type.gd
+++ unit/unit_type.gd
@@ -7,3 +7,5 @@ enum MovementType { NEIGHBORING_TILES }
 @export var max_hp := 10
 @export var movement_type := MovementType.NEIGHBORING_TILES
 @export var movement_range := 1
+@export var attack_type := MovementType.NEIGHBORING_TILES
+@export var attack_range := 1
