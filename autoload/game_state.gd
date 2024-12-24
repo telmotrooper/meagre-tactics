f6f55501da5b2c78ae662ebc05092f72ca12c13c
Telmo "Trooper <telmo.valverde@tarmac.io>
2024-12-24 17:37:21 -0300

    added sound effects for rotating board and ending turn (#21)
    * game state is now a scene
    
    * added sound effect when rotating board
    
    * added sound effect when turn ends

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -5,6 +5,8 @@ signal unit_hovered
 @warning_ignore("unused_signal")
 signal not_hovering_any_unit
 
+@export var end_turn_sound: AudioStream
+
 var board: Board
 var selected_unit: Unit
 var camera_pivot: CameraPivot
@@ -16,3 +18,8 @@ func end_turn() -> void:
 	current_turn = "blue" if current_turn == "red" else "red"
 	ui.update_turn()
 	selected_unit = null
+	play_sound(end_turn_sound)
+
+func play_sound(audio_stream: AudioStream) -> void:
+	$AudioStreamPlayer.stream = audio_stream
+	$AudioStreamPlayer.play()
