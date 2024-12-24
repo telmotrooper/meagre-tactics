8d81520028f080d48563e8668c413daf477e3898
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-23 21:20:30 -0300

    animated camera rotation

--- camera_pivot.gd
+++ camera_pivot.gd
@@ -1,4 +1,6 @@
 extends Node3D
 
 func rotate_camera(degrees: float) -> void:
-	rotation_degrees.y += degrees
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self, "rotation_degrees", rotation_degrees + Vector3(0,degrees,0), 0.5)
