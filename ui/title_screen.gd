b64fbf9556a956d0ef83ad246639d5daa122a82d
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-25 22:44:27 -0300

    added scene transitions

--- ui/title_screen.gd
+++ ui/title_screen.gd
@@ -3,4 +3,4 @@ extends Node3D
 @export var practice_scene: PackedScene
 
 func _on_practice_button_pressed() -> void:
-	get_tree().change_scene_to_packed(practice_scene)
+	SceneChanger.change_scene(practice_scene.get_path())
