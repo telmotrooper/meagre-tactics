b7dba6b91b6acbd652856dc8373a6b9054e5c82f
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-26 12:38:17 -0300

    added 'enter dungeon' animation to title screen

--- ui/title_screen/title_screen.gd
+++ ui/title_screen/title_screen.gd
@@ -3,4 +3,12 @@ extends Node3D
 @export var practice_scene: PackedScene
 
 func _on_practice_button_pressed() -> void:
-	SceneChanger.change_scene(practice_scene.get_path())
+	enter_dungeon()
+
+func enter_dungeon():
+	var tween := create_tween()
+	tween.tween_property($Control, "modulate", Color.hex(0xffffff00), 1.0)
+	tween.tween_property($Node3D/Camera3D, "position", Vector3(0,3,-8),3.5)
+	tween.tween_callback(func():
+		SceneChanger.change_scene(practice_scene.get_path())
+	)
