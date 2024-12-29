96b398a340e5267851be6c940d5940f8af2c210f
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-29 15:35:02 -0300

    turn timer changes color based on team color

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -9,15 +9,23 @@ const TITLE_SCREEN := "res://ui/title_screen/title_screen.tscn"
 
 @export var end_turn_sound: AudioStream
 
+var progress_bar_fill: StyleBoxFlat = load("res://ui/progress_bar_fill.tres")
+
 var board: Board
 var selected_unit: Unit
 var camera_pivot: CameraPivot
 var ui: UI
 
+const team_colors := {
+	"blue": "#384962",
+	"red": "#c44836"
+}
+
 var current_turn := "blue"
 
 func end_turn() -> void:
 	current_turn = "blue" if current_turn == "red" else "red"
+	progress_bar_fill.bg_color = team_colors[current_turn]
 	ui.update_turn()
 	selected_unit = null
 	play_sound(end_turn_sound)
