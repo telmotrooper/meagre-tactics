19c6fef883fc4b44c055bae911fe8abe700d8f18
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-29 23:08:15 -0300

    generate hover materials automatically

--- tile/tile.gd
+++ tile/tile.gd
@@ -7,15 +7,34 @@ enum State { REGULAR, REGULAR_HOVER, WALKABLE, WALKABLE_HOVER, ATTACKABLE, ATTAC
 
 @export_group("Materials")
 @export var tile_material: Material
-@export var hover_tile_material: Material
 @export var walk_tile_material: Material
-@export var hover_walk_tile_material: Material
 @export var debug_tile_material: Material
 
 @onready var ray_casts = %RayCasts
 
 var state := State.REGULAR
 var reached_through_enemy_tile = null
+
+var hover_tile_material: Material
+var hover_walk_tile_material: Material
+
+func _ready() -> void:
+	# TODO: Prepare hover materials only once, instead of on every tile.	
+	hover_tile_material = derive_material(tile_material)
+	hover_walk_tile_material = derive_material(walk_tile_material)
+
+func derive_material(existing_material: Material, factor := 0.2) -> Material:
+	# Derive a brighter version of an existing material, to be used when hovering a tile.
+	var new_material = StandardMaterial3D.new()
+	new_material.albedo_texture = load("res://tile/tile_texture.png")
+	
+	new_material.albedo_color = Color(
+		existing_material.albedo_color.r + factor,
+		existing_material.albedo_color.g + factor,
+		existing_material.albedo_color.b + factor
+	)
+	
+	return new_material
 
 func set_state(new_state: State) -> void:
 	state = new_state
