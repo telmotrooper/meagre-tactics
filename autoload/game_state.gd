7a27305628e67ca005286c504bd0d449f3bb71b0
Telmo Valverde <telmo.valverde@tarmac.io>
2024-12-30 23:48:53 -0300

    fixed typo

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -56,7 +56,7 @@ func consume_action(action: Action) -> void:
 	
 	action_consumed.emit(action)
 	
-	# TODO: Remove "end turn" when only "tur"n is available once "turn" is implemented.
+	# TODO: Remove "end turn" when only "turn" is available once "turn" is implemented.
 	if len(remaining_actions) == 0 or (len(remaining_actions) == 1 and remaining_actions[0] == Action.TURN):
 		end_turn()
 
