a6ad5a3872837fe99a31424a0e1e66111300ceae
Telmo Valverde <telmo.valverde@tarmac.io>
2025-01-04 22:46:52 -0300

    using collision shape from mesh

--- unit/arrow/arrow.gd
+++ unit/arrow/arrow.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$arrow/Plane.mouse_entered.connect(_on_mouse_entered)
+	$arrow/Plane.mouse_exited.connect(_on_mouse_exited)
+	$arrow/Plane.input_event.connect(_on_input_event)
 
+func _on_mouse_entered() -> void:
+	print("_on_mouse_entered")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_mouse_exited() -> void:
+	print("_on_mouse_exited")
+
+func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+		print("arrow clicked")
